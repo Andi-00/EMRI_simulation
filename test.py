22f2779e527ff483db63bd6e9bf82cbf1d1ce8a0
andi <andreasweber2001@outlook.de>
2023-06-25 12:10:10 +0200

    this was a test from the web

--- test.py
+++ test.py
@@ -1,2 +1,99 @@
+import sys
+import os
+
+from matplotlib import pyplot as plt
 import numpy as np
 
+from few.trajectory.inspiral import EMRIInspiral
+from few.amplitude.romannet import RomanAmplitude
+from few.amplitude.interp2dcubicspline import Interp2DAmplitude
+from few.waveform import FastSchwarzschildEccentricFlux, SlowSchwarzschildEccentricFlux, GenerateEMRIWaveform
+from few.utils.utility import (get_overlap,
+                               get_mismatch,
+                               get_fundamental_frequencies,
+                               get_separatrix,
+                               get_mu_at_t,
+                               get_p_at_t,
+                               get_kerr_geo_constants_of_motion,
+                               xI_to_Y,
+                               Y_to_xI)
+
+from few.utils.ylm import GetYlms
+from few.utils.modeselector import ModeSelector
+from few.summation.interpolatedmodesum import CubicSplineInterpolant
+from few.waveform import SchwarzschildEccentricWaveformBase
+from few.summation.interpolatedmodesum import InterpolatedModeSum
+from few.summation.directmodesum import DirectModeSum
+from few.utils.constants import *
+from few.summation.aakwave import AAKSummation
+from few.waveform import Pn5AAKWaveform, AAKWaveformBase
+
+use_gpu = False
+
+# keyword arguments for inspiral generator (RunSchwarzEccFluxInspiral)
+inspiral_kwargs={
+        "DENSE_STEPPING": 0,  # we want a sparsely sampled trajectory
+        "max_init_len": int(1e3),  # all of the trajectories will be well under len = 1000
+    }
+
+# keyword arguments for inspiral generator (RomanAmplitude)
+amplitude_kwargs = {
+    "max_init_len": int(1e3),  # all of the trajectories will be well under len = 1000
+    "use_gpu": use_gpu  # GPU is available in this class
+}
+
+# keyword arguments for Ylm generator (GetYlms)
+Ylm_kwargs = {
+    "assume_positive_m": False  # if we assume positive m, it will generate negative m for all m>0
+}
+
+# keyword arguments for summation generator (InterpolatedModeSum)
+sum_kwargs = {
+    "use_gpu": use_gpu,  # GPU is availabel for this type of summation
+    "pad_output": False,
+}
+
+# THE FOLLOWING THREAD COMMANDS DO NOT WORK ON THE M1 CHIP, BUT CAN BE USED WITH OLDER MODELS
+# EVENTUALLY WE WILL PROBABLY REMOVE OMP WHICH NOW PARALLELIZES WITHIN ONE WAVEFORM AND LEAVE IT TO
+# THE USER TO PARALLELIZE FOR MANY WAVEFORMS ON THEIR OWN.
+
+# set omp threads one of two ways
+# num_threads = 4
+
+# this is the general way to set it for all computations
+# from few.utils.utility import omp_set_num_threads
+# omp_set_num_threads(num_threads)
+
+few = FastSchwarzschildEccentricFlux(
+    inspiral_kwargs=inspiral_kwargs,
+    amplitude_kwargs=amplitude_kwargs,
+    Ylm_kwargs=Ylm_kwargs,
+    sum_kwargs=sum_kwargs,
+    use_gpu=use_gpu,
+    # num_threads=num_threads,  # 2nd way for specific classes
+)
+
+
+
+# parameters
+M = 1e6
+mu = 1e2
+p0 = 12.0
+e0 = 0.4
+theta = np.pi/3  # polar viewing angle
+phi = np.pi/4  # azimuthal viewing angle
+dt = 10.0
+
+wave = few(M, mu, p0, e0, theta, phi, dt=dt, T=1.0)  #  assumes dt = 10.0 for max T = 1.0 year
+
+
+
+
+dt = 10.0
+t = np.arange(len(wave)) * dt
+
+plt.plot(wave.real[:1000])
+plt.plot(wave.imag[:1000])
+
+
+plt.savefig("test.png")
